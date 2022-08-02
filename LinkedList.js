9a32394a4c14151cc2469263aa219bfb33dfa436
HassanKhan123 <hk2602809@gmail.com>
2022-08-02 13:07:05 +0500

    added: remove and insert method in linked list

--- LinkedList.js
+++ LinkedList.js
@@ -8,6 +8,12 @@
 //   },
 // };
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -19,10 +25,7 @@ class LinkedList {
   }
 
   prepend(value) {
-    const newNode = {
-      value,
-      next: null,
-    };
+    const newNode = new Node(value);
 
     newNode.next = this.head;
     this.head = newNode;
@@ -31,16 +34,59 @@ class LinkedList {
     return this;
   }
 
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
   append(value) {
-    const newNode = {
-      value,
-      next: null,
-    };
+    const newNode = new Node(value);
 
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
   }
 }
 
@@ -48,4 +94,7 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+myLinkedList.remove(2);
+const list = myLinkedList.printList();
+console.log(list);
